03cadbca241a78463f8f1586e6f349d7d54672c9
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-09-28 17:57:02 -0300

    Criado a automação para teste de apontamento de saida e entrada

--- cypress/e2e/time-at-work/punchinout.cy.js
+++ cypress/e2e/time-at-work/punchinout.cy.js
@@ -1,6 +1,8 @@
 context('Realizar apontamento de inicio e fim de expediente.', () => {
    beforeEach(() => {
-     cy.login()     
+    //  cy.session('loginSession', ()=> {
+        cy.login() 
+    //  })    
     })
     it('Realizar apontamento de entrada', () => {
         //act
@@ -8,9 +10,9 @@ context('Realizar apontamento de inicio e fim de expediente.', () => {
         cy.get('.oxd-topbar-body-nav > ul > :nth-child(2)').click()
         cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-link').click()
         cy.get('.oxd-date-input > .oxd-input').clear()
-        cy.get('.oxd-date-input > .oxd-input').type('2025-21-09')
+        cy.get('.oxd-date-input > .oxd-input').type('2025-25-09')
         cy.get('.oxd-time-input > .oxd-input').clear()
-        cy.get('.oxd-time-input > .oxd-input').type('11:41 AM')
+        cy.get('.oxd-time-input > .oxd-input').type('01:00 PM')
         cy.get('.orangehrm-card-container').click()
         cy.get('.oxd-textarea').focus()
         cy.get('.oxd-textarea').type('Teste de apontamento de entrada 09/21/2025')
@@ -22,5 +24,41 @@ context('Realizar apontamento de inicio e fim de expediente.', () => {
         cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/attendance/punchOut')
 
         
-    })
+    }),
+    it.only('Realizar o apontamento de saida', () => {
+        cy.pathPunch()
+        cy.get('.orangehrm-card-container > .oxd-text--h6').then(($el) => {const texto = $el.text()  
+            // pega o texto do elemento
+
+        if (texto.includes('Punch In')) {
+            cy.log('O status atual é Punch In')
+            cy.punchIn()
+            cy.get('.oxd-date-input > .oxd-input').should('be.visible')
+            cy.get('.oxd-date-input > .oxd-input').clear()
+            cy.get('.oxd-date-input > .oxd-input').type('2025-27-09')
+            cy.get('.oxd-time-input > .oxd-input').clear()
+            cy.get('.oxd-time-input > .oxd-input').type('05:00 PM')
+            cy.get('.orangehrm-card-container').click()
+            cy.get('.oxd-textarea').focus()
+            cy.get('.oxd-textarea').type('Teste de apontamento de saida 09/26/2025')
+            cy.wait(3000)
+            cy.get('.oxd-button').click()
+        } else if (texto.includes('Punch Out')) {
+            cy.log('O status atual é Punch Out')
+            cy.get('.oxd-date-input > .oxd-input').should('be.visible')
+            cy.get('.oxd-date-input > .oxd-input').clear()
+            cy.get('.oxd-date-input > .oxd-input').type('2025-27-09')
+            cy.get('.oxd-time-input > .oxd-input').clear()
+            cy.get('.oxd-time-input > .oxd-input').type('05:00 PM')
+            cy.get('.orangehrm-card-container').click()
+            cy.get('.oxd-textarea').focus()
+            cy.get('.oxd-textarea').type('Teste de apontamento de saida 09/26/2025')
+            cy.wait(3000)
+            cy.get('.oxd-button').click()
+        } else {
+            cy.log('Nenhum dos dois status encontrados')
+        }
+        })
+
+})
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,6 +5,27 @@ Cypress.Commands.add('login', (username = 'Admin', password = 'admin123') => {
   cy.get('.oxd-button').click()
 })
 
-// Cypress.Commands.add('PunchInOut', () => {
-//   cy.
-// })
+Cypress.Commands.add('pathPunch', () => {
+  cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+
+  cy.get(':nth-child(4) > .oxd-main-menu-item').click()
+  cy.get('.oxd-topbar-body-nav > ul > :nth-child(2)').click()
+  cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-link').click()
+
+})
+
+Cypress.Commands.add('punchIn', () => {
+  cy.pathPunch()
+  cy.get(':nth-child(4) > .oxd-main-menu-item').click()
+  cy.get('.oxd-topbar-body-nav > ul > :nth-child(2)').click()
+  cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-link').click()
+  cy.get('.oxd-date-input > .oxd-input').clear()
+  cy.get('.oxd-date-input > .oxd-input').type('2025-25-09')
+  cy.get('.oxd-time-input > .oxd-input').clear()
+  cy.get('.oxd-time-input > .oxd-input').type('01:00 PM')
+  cy.get('.orangehrm-card-container').click()
+  cy.get('.oxd-textarea').focus()
+  cy.get('.oxd-textarea').type('Teste de apontamento de entrada 09/21/2025')
+  cy.get('.oxd-button').click()
+  cy.wait(5000)
+})
